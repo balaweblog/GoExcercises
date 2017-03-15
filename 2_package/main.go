ddcad951e30fd4109905217f2a51e83fe3287eb3
Balamurugan Alagumalai <balamurugan.alagumalai@gmail.com>
2017-03-15 09:46:19 +0530

    packages

---
+++ 2_package/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"stringpack"
+	"varpack"
+)
+
+// add this path in go path C:\Balamurugan\CodeBase\2_package
+func main() {
+	fmt.Println(stringpack.Reverse("test"))
+	fmt.Println(varpack.First)
+	//fmt.Println(varpack.second)
+}
---
+++ 2_package/src/varpack/varpack.go
@@ -0,0 +1,5 @@
+package varpack
+
+// MyName will be exported because it starts with a capital letter.
+var First = "first"
+var second = "second"
